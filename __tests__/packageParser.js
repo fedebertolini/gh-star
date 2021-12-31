b1bb58454b1c2e5dc50e59c844ca7368f45cfad0
Federico Bertolini <fedebertolini@gmail.com>
2021-12-31 13:41:50 -0300

    replace jest with uvu

--- __tests__/packageParser.js
+++ __tests__/packageParser.js
@@ -1,108 +1,110 @@
-const parser = require('../src/packageParser');
+const { test } = require("uvu");
+const assert = require("uvu/assert");
+const parser = require("../src/packageParser");
 
-describe('package parser', () => {
-    it('returns an ordered array of the package\'s production dependencies', () => {
-        const result = parser.parsePackage({
-            name: 'gh-star',
-            version: '0.0.1',
-            dependencies: {
-                'npm-registry-client': '^7.4.5',
-                'octonode': '^0.7.8'
-            },
-        });
+test("returns an ordered array of the package's production dependencies", () => {
+  const result = parser.parsePackage({
+    name: "gh-star",
+    version: "0.0.1",
+    dependencies: {
+      "npm-registry-client": "^7.4.5",
+      octonode: "^0.7.8",
+    },
+  });
 
-        expect(result).toEqual([
-            {
-                name: 'npm-registry-client',
-                version: '^7.4.5',
-            },
-            {
-                name: 'octonode',
-                version: '^0.7.8',
-            },
-        ]);
-    });
-
-    it('returns an ordered array of the package\'s dev dependencies', () => {
-        const result = parser.parsePackage({
-            name: 'gh-star',
-            version: '0.0.1',
-            devDependencies: {
-                'jest': '^18.1.0',
-                'eslint-loader': '^1.6.1',
-                'eslint': '^3.13.0',
-                'eslint-config-airbnb-base': '^11.0.0'
-            },
-        });
-
-        expect(result).toEqual([
-            {
-                name: 'eslint',
-                version: '^3.13.0',
-            },
-            {
-                name: 'eslint-config-airbnb-base',
-                version: '^11.0.0',
-            },
-            {
-                name: 'eslint-loader',
-                version: '^1.6.1',
-            },
-            {
-                name: 'jest',
-                version: '^18.1.0',
-            },
-        ]);
-    });
-
-    it('returns an ordered array of all the package\'s dependencies', () => {
-        const result = parser.parsePackage({
-            name: 'gh-star',
-            version: '0.0.1',
-            dependencies: {
-                'npm-registry-client': '^7.4.5',
-                'octonode': '^0.7.8'
-            },
-            devDependencies: {
-                'jest': '^18.1.0',
-                'eslint-loader': '^1.6.1',
-                'eslint': '^3.13.0',
-                'eslint-config-airbnb-base': '^11.0.0'
-            },
-        });
-
-        expect(result).toEqual([
-            {
-                name: 'eslint',
-                version: '^3.13.0',
-            },
-            {
-                name: 'eslint-config-airbnb-base',
-                version: '^11.0.0',
-            },
-            {
-                name: 'eslint-loader',
-                version: '^1.6.1',
-            },
-            {
-                name: 'jest',
-                version: '^18.1.0',
-            },
-            {
-                name: 'npm-registry-client',
-                version: '^7.4.5',
-            },
-            {
-                name: 'octonode',
-                version: '^0.7.8',
-            },
-        ]);
-    });
-
-    it('parses this project\'s package.json', () => {
-        const result = parser.parsePackage('../package.json');
-
-        expect(typeof result).toBe('object');
-        expect(result.length).toBeGreaterThan(2);
-    });
+  assert.equal(result, [
+    {
+      name: "npm-registry-client",
+      version: "^7.4.5",
+    },
+    {
+      name: "octonode",
+      version: "^0.7.8",
+    },
+  ]);
 });
+
+test("returns an ordered array of the package's dev dependencies", () => {
+  const result = parser.parsePackage({
+    name: "gh-star",
+    version: "0.0.1",
+    devDependencies: {
+      jest: "^18.1.0",
+      "eslint-loader": "^1.6.1",
+      eslint: "^3.13.0",
+      "eslint-config-airbnb-base": "^11.0.0",
+    },
+  });
+
+  assert.equal(result, [
+    {
+      name: "eslint",
+      version: "^3.13.0",
+    },
+    {
+      name: "eslint-config-airbnb-base",
+      version: "^11.0.0",
+    },
+    {
+      name: "eslint-loader",
+      version: "^1.6.1",
+    },
+    {
+      name: "jest",
+      version: "^18.1.0",
+    },
+  ]);
+});
+
+test("returns an ordered array of all the package's dependencies", () => {
+  const result = parser.parsePackage({
+    name: "gh-star",
+    version: "0.0.1",
+    dependencies: {
+      "npm-registry-client": "^7.4.5",
+      octonode: "^0.7.8",
+    },
+    devDependencies: {
+      jest: "^18.1.0",
+      "eslint-loader": "^1.6.1",
+      eslint: "^3.13.0",
+      "eslint-config-airbnb-base": "^11.0.0",
+    },
+  });
+
+  assert.equal(result, [
+    {
+      name: "eslint",
+      version: "^3.13.0",
+    },
+    {
+      name: "eslint-config-airbnb-base",
+      version: "^11.0.0",
+    },
+    {
+      name: "eslint-loader",
+      version: "^1.6.1",
+    },
+    {
+      name: "jest",
+      version: "^18.1.0",
+    },
+    {
+      name: "npm-registry-client",
+      version: "^7.4.5",
+    },
+    {
+      name: "octonode",
+      version: "^0.7.8",
+    },
+  ]);
+});
+
+test("parses this project's package.json", () => {
+  const result = parser.parsePackage("../package.json");
+
+  assert.type(result, "object");
+  assert.equal(result.length > 2, true);
+});
+
+test.run();
--- __tests__/packageRepoResolver.js
+++ __tests__/packageRepoResolver.js
@@ -1,55 +1,63 @@
-const resolver = require('../src/packageRepoResolver');
+const { test } = require("uvu");
+const assert = require("uvu/assert");
+const resolver = require("../src/packageRepoResolver");
 
-describe('package repo resolver', () => {
-    it('gets the repo\.s url from the package definition', () => {
-        const packageDefinition = {
-            name: 'gh-star',
-            repository: {
-                type: 'git',
-                url: 'git+https://github.com/fedebertolini/gh-star.git',
-            },
-        };
+test("gets the repo.s url from the package definition", () => {
+  const packageDefinition = {
+    name: "gh-star",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/fedebertolini/gh-star.git",
+    },
+  };
 
-        const repo = resolver.getRepoFromPackage(packageDefinition);
-        expect(repo).toEqual({
-            fullName: 'fedebertolini/gh-star',
-            repository: 'gh-star',
-            username: 'fedebertolini',
-        });
-    });
-
-    it('parses valid git urls', () => {
-        repos = [{
-            url: 'https://github.com/svg/svgo.git',
-            fullName: 'svg/svgo',
-        }, {
-            url: 'git+https://github.com/sboudrias/Inquirer.js.git',
-            fullName: 'sboudrias/inquirer.js',
-        }, {
-            url: 'git+https://github.com/fedebertolini/gh-star.git',
-            fullName: 'fedebertolini/gh-star',
-        }, {
-            url: 'https://github.com/svg/svgo',
-            fullName: 'svg/svgo',
-        }, {
-            url: 'git+https://github.com/sboudrias/Inquirer.js.git#v3.0.1',
-            fullName: 'sboudrias/inquirer.js',
-        }];
-
-        repos.forEach(repo => {
-            const repoInfo = resolver.parseGitUrl(repo.url);
-            expect(repoInfo.fullName).toBe(repo.fullName);
-        });
-    });
-
-    it('parses invalid git urls', () => {
-        repos = [
-            'https://bitbucket.org/simpletechs/node-safe-enum.git',
-            'https://github.com/fedebertolini/',
-        ];
-
-        repos.forEach(url => {
-            expect(resolver.parseGitUrl(url)).toBe(null);
-        });
-    });
+  const repo = resolver.getRepoFromPackage(packageDefinition);
+  assert.equal(repo, {
+    fullName: "fedebertolini/gh-star",
+    repository: "gh-star",
+    username: "fedebertolini",
+  });
 });
+
+test("parses valid git urls", () => {
+  repos = [
+    {
+      url: "https://github.com/svg/svgo.git",
+      fullName: "svg/svgo",
+    },
+    {
+      url: "git+https://github.com/sboudrias/Inquirer.js.git",
+      fullName: "sboudrias/inquirer.js",
+    },
+    {
+      url: "git+https://github.com/fedebertolini/gh-star.git",
+      fullName: "fedebertolini/gh-star",
+    },
+    {
+      url: "https://github.com/svg/svgo",
+      fullName: "svg/svgo",
+    },
+    {
+      url: "git+https://github.com/sboudrias/Inquirer.js.git#v3.0.1",
+      fullName: "sboudrias/inquirer.js",
+    },
+  ];
+
+  repos.forEach((repo) => {
+    const repoInfo = resolver.parseGitUrl(repo.url);
+    assert.equal(repoInfo.fullName, repo.fullName);
+  });
+});
+
+test("parses invalid git urls", () => {
+  repos = [
+    "https://bitbucket.org/simpletechs/node-safe-enum.git",
+    "https://github.com/fedebertolini/",
+  ];
+
+  repos.forEach((url) => {
+    assert.equal(resolver.parseGitUrl(url), null);
+  });
+});
+
+test.run();
